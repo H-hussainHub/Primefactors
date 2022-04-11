957580980e4ffa80b4e8b802de1835e0001e5581
H-hussainHub <H-hussainHub@users.noreply.github.com>
2022-04-11 02:22:01 +0100

    Setup the MVVM structure, populated each folder & created the bindings

---
+++ Primefactors/ViewModels/EratosthenesViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Primefactors.ViewModels
+{
+    class EratosthenesViewModel : IEnumerable<int>
+    {
+        // Storing the prime numbers within a collection
+        private static List<int> _primes = new List<int>();
+        private int _lastChecked;
+
+
+        public EratosthenesViewModel()
+        {
+            _primes.Add(2);
+            _lastChecked = 2;
+        }
+
+
+        // Method to check if a number is prime which returns a boolean value that is true/false
+        private bool IsPrime(int checkValue)
+        {
+            bool isPrime = true;
+
+            foreach (int prime in _primes)
+            {
+                if ((checkValue % prime) == 0 && prime <= Math.Sqrt(checkValue))
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (int prime in _primes)
+                yield return prime;
+
+            while (_lastChecked < int.MaxValue)
+            {
+                _lastChecked++;
+
+                if (IsPrime(_lastChecked))
+                {
+                    _primes.Add(_lastChecked);
+                    yield return _lastChecked;
+                }
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
---
+++ Primefactors/ViewModels/PrimeViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Primefactors.ViewModels
+{
+    class PrimeViewModel : ViewModelBase
+    {
+        private int _factorise;
+        public int Factorise
+        {
+            get
+            {
+                return _factorise;
+            }
+            set
+            {
+                _factorise = value;
+                OnPropertyChanged(nameof(Factorise));
+            }
+        }
+
+        private int _primeFactors;
+        public int PrimeFactors
+        {
+            get
+            {
+                return _primeFactors;
+            }
+            set
+            {
+                _primeFactors = value;
+                OnPropertyChanged(nameof(PrimeFactors));
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+
+        public ICommand CancelCommand { get; }
+
+    }
+}
